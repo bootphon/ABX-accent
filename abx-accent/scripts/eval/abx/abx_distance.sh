7d3bf6d1d3d778d8f3346d67704212fcd43b981a
manelkhentout <manelkhentoutmk@gmail.com>
2022-09-15 11:24:39 +0200

    abx distance script update

--- abx-accent/scripts/eval/abx/abx_distance.sh
+++ abx-accent/scripts/eval/abx/abx_distance.sh
@@ -1,36 +1,21 @@
 #!/bin/bash
 #This for full run of the ABX pipeline in command line
 
-#  AESRC Corpus #
-#   American	#
-#   British	    #
-#   Canadian	#
-#   Chinese	    #
-#   Indian      #
-#   Japanese	#
-#   Korean      #
-#   Spanish	    #
-#	Portuguese	#
-#   Russian	    #
-#################
-
-echo "$(hostname)"
 
 #AESRC corpus
 declare -a accents=("American" "Japanese" "British" "Canadian" "Chinese" "Indian" "Korean" "Spanish" "Portuguese" "Russian")
 
 #parametres
-results="/scratch2/mkhentout/AESRC_2H/results/test/"
+results= $1
 abx="/abx"
 item_file="/item_file.item"
 phone="/abx_task1.abx"
 distance_file="/distance1.distance"
-score_file="/score1.score"
-analyse_file="/task1.csv"
+score_file="/score.score"
+analyse_file="/task.csv"
 features_file="/h5_file.h5f"
 
 for accent in ${accents[@]};do
-
     output="$results$accent$abx";
 
     #item_file
