83b79d6e5691089b2b365835e7011a8bbcf009ab
Manel khentout <35903456+manelkhentout@users.noreply.github.com>
2022-09-22 14:18:45 +0200

    Update across_task.sh

--- abx-accent/scripts/evals/generate_abx_score/across_task.sh
+++ abx-accent/scripts/evals/generate_abx_score/across_task.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --nodelist=puck5
 
-#
-# This is for the ABX task "on phone" used for the AESRC corpus
-#
+# loading modules and activating the right conda env
+source #your_source
+module load espeak
+conda activate abx_aesrc
+
+
 
 #AESRC corpus
 declare -a accents=("American" "British" "Canadian" "Chinese" "Indian" "Japanese" "Korean" "Spanish" "Portuguese" "Russian")
