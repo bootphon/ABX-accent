27456b48ebb540173ef6713d356ac5356922de1b
Manel khentout <35903456+manelkhentout@users.noreply.github.com>
2022-09-22 14:45:09 +0200

    Create abx_analyze.sh

---
+++ abx-accent/scripts/evals/generate_abx_score/abx_analyze.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#This for full run of the ABX pipeline in command line
+
+
+#AESRC corpus
+declare -a accents=("American" "Japanese" "British" "Canadian" "Chinese" "Indian" "Korean" "Spanish" "Portuguese" "Russian")
+
+#parametres
+results= $1
+abx="/abx"
+item_file="/item_file.item"
+phone="/abx_task.abx"
+distance_file="/distance.distance"
+score_file="/score.score"
+analyse_file="/task.csv"
+features_file="/h5_file.h5f"
+
+for accent in ${accents[@]};do
+    output="$results$accent$abx";
+
+    #item_file
+    item="$output$item_file";
+    task="$output$phone";
+    analyze="$output$analyse_file";
+    features="$output$features_file";
+
+    # collapsing the results
+    abx-analyze $score $task $analyze
+
+done
