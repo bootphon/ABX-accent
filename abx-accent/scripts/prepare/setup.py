54428ca71035579aab4d4459308dbc1ec1bf2130
Manel khentout <manelkhentoutmk@gmail.com>
2025-04-14 13:31:29 +0200

    Update setup.py

--- abx-accent/scripts/prepare/setup.py
+++ abx-accent/scripts/prepare/setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright KHENTOUT Manel, Coml
+# Copyright KHENTOUT Manel, CoML
 #
 # This file is part of prepare step: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -8,24 +8,26 @@
 #
 # This is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with abkahzia. If not, see <http://www.gnu.org/licenses/>.
-
+# along with abkhazia. If not, see <http://www.gnu.org/licenses/>.
 """Setup script for the abkhazia package"""
 
 import os
 import abkhazia
 from setuptools import setup, find_packages
 
+# Constants and configuration
 GITHUB_BOOTPHON = 'https://github.com/bootphon/'
 SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
 VERSION = abkhazia.__version__
 
-# On Reads The Docs we don't install any package
+# Check if running on ReadTheDocs
 ON_RTD = os.environ.get('READTHEDOCS', None) == 'True'
+
+# Dependencies
 REQUIREMENTS = [] if ON_RTD else [
     'numpy',
     'progressbar2',
@@ -38,29 +40,44 @@ REQUIREMENTS = [] if ON_RTD else [
     'matplotlib'
 ]
 
+# Read long description from README
+with open('README.md', 'r') as readme_file:
+    long_description = readme_file.read()
+
 setup(
+    # Package information
     name='abkhazia',
     version=VERSION,
     packages=find_packages(exclude=['test']),
     zip_safe=False,
-    scripts=[],
-
-    # install python dependencies from PyPI
+    
+    # Dependencies
     install_requires=REQUIREMENTS,
-
-    # include any files in abkhazia/share and abkhazia.conf
+    
+    # Data files and resources
     package_data={'abkhazia': ['share/*.*']},
-
-    # define the command-line script to use
-    entry_points={'console_scripts': [
-        'abkhazia = abkhazia.commands.abkhazia_main:main']},
-
-    # metadata for upload to PyPI
+    
+    # Entry points
+    entry_points={
+        'console_scripts': [
+            'abkhazia = abkhazia.commands.abkhazia_main:main'
+        ]
+    },
+    
+    # Metadata for PyPI
     author='Thomas Schatz, Mathieu Bernard, Manel Khentout, Roland Thiolliere, Xuan-Nga Cao',
     author_email='manelkhentoutmk@gmail.com',
     description='ABX and Kaldi experiments on speech corpora made easy',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='GPL3',
     keywords='speech corpus ASR kaldi ABX',
     url='https://github.com/bootphon/abkhazia',
-    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    python_requires='>=3.6',
 )
